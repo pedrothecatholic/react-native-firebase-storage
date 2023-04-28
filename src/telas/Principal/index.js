902169a6d316bc77d3a64d8a03978b5a35bb98cf
pedrothecatholic <pedro.sousa21@outlook.com>
2023-04-28 14:09:05 -0300

    Primeiro teste de adição de imagens

--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,36 +1,50 @@
-import { View, ScrollView } from "react-native";
-import { useEffect, useState } from "react";
-import { Cabecalho } from "../../componentes/Cabecalho";
-import { CartaoInfo } from "../../componentes/CartaoInfo";
-import { NovoPostBotao } from "../../componentes/NovoPostBotao";
-import { pegarPostsTempoReal } from "../../servicos/firestore";
-import estilos from "./estilos";
+import { View, ScrollView, Image } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Cabecalho } from '../../componentes/Cabecalho';
+import { CartaoInfo } from '../../componentes/CartaoInfo';
+import { NovoPostBotao } from '../../componentes/NovoPostBotao';
+import { pegarPostsTempoReal } from '../../servicos/firestore';
+import estilos from './estilos';
+import { Storage, storage } from '../../config/firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
 
 export default function Principal({ navigation }) {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        pegarPostsTempoReal(setPosts);
-    },[])
+  useEffect(() => {
+    const imagemRef = ref(storage, 'img3.png');
+    getDownloadURL(imagemRef).then((url) => {
+      console.log(url);
+    });
 
-    return (
-        <View style={estilos.container}>
-            <Cabecalho />
-            <ScrollView style={estilos.scroll} showsVerticalScrollIndicator={false}>
+    pegarPostsTempoReal(setPosts);
+  }, []);
 
-                {posts?.map((item) => (
-                    <CartaoInfo 
-                        key={item.id} 
-                        imagem={item.imagem}
-                        titulo={item.titulo}  
-                        fonte={item.fonte} 
-                        descricao={item.descricao} 
-                        acao={() => navigation.navigate("Post", { item })}
-                    />
-                ))}
-            </ScrollView>
+  return (
+    <View style={estilos.container}>
+      <Cabecalho />
 
-            <NovoPostBotao acao={() => navigation.navigate("Post")} />
-        </View>
-    );
+      <Image
+        source={{
+          uri: 'https://firebasestorage.googleapis.com/v0/b/spaceapp-65096.appspot.com/o/img3.png?alt=media&token=f784b320-06d5-4bf4-9b51-316eec4e3a2d'
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+
+      <ScrollView style={estilos.scroll} showsVerticalScrollIndicator={false}>
+        {posts?.map((item) => (
+          <CartaoInfo
+            key={item.id}
+            imagem={item.imagem}
+            titulo={item.titulo}
+            fonte={item.fonte}
+            descricao={item.descricao}
+            acao={() => navigation.navigate('Post', { item })}
+          />
+        ))}
+      </ScrollView>
+
+      <NovoPostBotao acao={() => navigation.navigate('Post')} />
+    </View>
+  );
 }
